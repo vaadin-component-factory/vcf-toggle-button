36887bd08059a5e7cd5362248caf28eebb2ca78c
Sohrab Taee <sohrab.taee@gmail.com>
2019-09-03 14:00:07 +0300

    Added publish script.

--- src/vcf-toggle-button.js
+++ src/vcf-toggle-button.js
@@ -31,7 +31,7 @@ class VcfToggleButton extends ElementMixin(ThemableMixin(PolymerElement)) {
   }
 
   static get version() {
-    return '0.1.5';
+    return '0.1.6';
   }
 
   static get properties() {

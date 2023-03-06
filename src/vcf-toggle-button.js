22542bbecc55841de08f34045f24ad1379eb5a9d
Bruno Rezende <brunovianarezende@gmail.com>
2023-03-06 14:36:10 -0300

    Change dependencies versions to match vaadin 24

--- src/vcf-toggle-button.js
+++ src/vcf-toggle-button.js
@@ -1,7 +1,7 @@
 import { html, PolymerElement } from '@polymer/polymer/polymer-element';
 import { ThemableMixin } from '@vaadin/vaadin-themable-mixin';
 import { ElementMixin } from '@vaadin/vaadin-element-mixin';
-import '@vaadin/vaadin-checkbox';
+import '@vaadin/checkbox';
 
 class VcfToggleButton extends ElementMixin(ThemableMixin(PolymerElement)) {
   static get template() {
